67e17fa658e31a1dce63925129ce74f5f780b1d5
jihyun <cat2998@naver.com>
2024-08-06 00:33:51 +0900

    modify

--- 5_algorithm/13398.cpp
+++ 5_algorithm/13398.cpp
@@ -2,5 +2,23 @@
 using namespace std;
 
 int main(void) {
-    
+    int n;
+
+    cin >> n;
+
+    int arr[n];
+    int dp[n][2];
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    int ans = arr[0];
+    for (int i = 0; i < n; i++) {
+        dp[i][0] = dp[i][1] = arr[i];
+        if (i == 0)
+            continue;
+        dp[i][0] = max(dp[i - 1][0] + arr[i], arr[i]);
+        dp[i][1] = max(dp[i - 1][0], dp[i - 1][1] + arr[i]);
+        ans = max(ans, max(dp[i][0], dp[i][1]));
+    }
+    cout << ans;
 }
